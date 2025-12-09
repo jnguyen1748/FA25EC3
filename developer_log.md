aa447fc0c812e3b21e9029d5a7c2cfee9ade2035
Jyustin <supercatsd@gmail.com>
2025-12-09 10:56:21 -0800

    some edits to tree.h

---
+++ developer_log.md
@@ -0,0 +1,15 @@
+Your Developer Log must include:
+- A dated entry every time you work on the assignment
+- A description of what you worked on
+- Any bugs, issues, or problems you encountered
+- How you attempted to solve each issue
+- A brief explanation of every commit you make
+- A list of all files modified during each commit
+
+Commit messages must also be descriptive, and the Developer Log must reflect the reasoning behind each commit.
+
+2025/12/08
+worked on the tree.h for now and getting it put together, to understand how the project structure worked
+
+This work extended into the next day, so the commit is on 12/09
+
--- tree.h
+++ tree.h
@@ -39,29 +39,97 @@ public:
 
     // TODO: Write constructor
     // Node(const string &nodeID, const U &value);
+
+// note this will set children to empty
+Node(const string &nodeID, const U &value) :
+    id(nodeID), data(value), children() {
+}
+
 };
 
+
+
+       // TODO: Initialize root pointer to nullptr
+// we will do this in the tree constructor
 template <typename T>
 class Tree {
 private:
     Node<T>* root;
+    Node<T>* findNode2ndpart(Node<T>* currNode, const string &id) {
+        // 1st case for just null ptr
+        if (currNode == nullptr)
+            return nullptr;
+        // our base case
+        if (currNode->id == id)
+            return currNode;
+
+        // use this method upon each child (hence the recursion)
+        // so we just check each child with a recursion of findNode
+        for (Node<T>* child : currNode->children) {
+            Node<T>* result = findNode2ndpart(child, id);
+            if (result != nullptr)
+                return result;
+        }
+
+        return nullptr;
+    }
 
 public:
-    Tree();
-    // TODO: Initialize root pointer to nullptr
-
-    void createRoot(const string &id, const T &value);
+    // now we actually set the root ptr to null
+    Tree() : root(nullptr) {}
     // TODO: Allocate memory, assign id, assign data, set as root
+    void createRoot(const string &id, const T &value){
+if(root != nullptr) {
+    cout << "root exists" << endl;
+    return;
+}
 
-    void addNode(const string &parentID, const string &childID, const T &value);
+root = new Node<T>(id, value);
+// new root
+}
+
+
     // TODO: Find parent, create child, link parent to child
     // TODO: Support repeated children under multiple parents
 
-    Node<T>* findNode(const string &id);
-    // TODO: Use DFS or BFS to search tree
+void addNode(const string &parentID, const string &childID, const T &value){
+// 1. find parent node
+Node<T>* parentNode = findNode(parentID);
+if(parentNode == nullptr){
+cout <<"error:null parent" << endl;
+return;
+}
+// create new child node using id and value
+Node<T>* childNode = new Node<T>(childID, value);
+// connect child to parent
+parentNode->children.push_back(childNode);
+}
 
-    void printAll();
-    // TODO: Print entire structure in readable form
+
+Node<T>* findNode(const string &id) {
+        // TODO: Use DFS or BFS to search tree
+        // we will use DFS recursive search, which we will use a different method for
+        // this is so we can change the method headers
+        return findNode2ndpart(root, id);
+    }
+
+    void printAll(Node<T>* node, int depth) {
+        // TODO: Print entire structure in readable form
+        // 1st easy case
+        if(node == nullptr) {
+            return ;
+        }
+        // 1st for loop: will allow us to go through depth
+        for ( int i = 0; i < depth; i++) {
+            // first print the id of our node
+            cout << " " << node->id << ": " << endl;
+            // then call this to then print all of the children
+            // note that each child will print itself then go for children in depth
+            for (Node<T>* child : node->children) {
+                printAll(child, depth + 1);
+            }
+        }
+    }
 
     ~Tree();
     // TODO: Free all allocated memory
